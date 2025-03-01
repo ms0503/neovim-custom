884b346a6ba354fb047d6c0c8a481f6025d50350
Sora Tonami <ms0503@outlook.com>
2025-03-02 01:55:34 +0900

    chore: add modules

--- flake.nix
+++ flake.nix
@@ -50,103 +50,125 @@
       treefmt-nix,
       ...
     }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      flake.overlays = import ./overlays.nix;
-      imports = [
-        git-hooks.flakeModule
-        treefmt-nix.flakeModule
-      ];
-      perSystem =
-        {
-          config,
-          inputs',
-          system,
-          ...
-        }:
-        let
-          inherit (inputs') neovim-nightly-overlay;
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              fenix.overlays.default
-              self.overlays.default
-            ];
-          };
-        in
-        {
-          _module.args.pkgs = pkgs;
-          devShells.default =
-            let
-              scripts = with pkgs; [
-                (writeScriptBin "update-srcs" ''
-                  if [[ ! -f $PWD/flake.nix ]]; then
-                    printf "Please run in flake root\n"
-                    exit 1
-                  fi
-                  ${pkgs.nvfetcher}/bin/nvfetcher
-                  ${pkgs.node2nix}/bin/node2nix \
-                    -i pkgs/nodePackages/pkg-list.json \
-                    -o pkgs/nodePackages/pkgs.nix \
-                    -c pkgs/nodePackages/cmp.nix \
-                    -e pkgs/nodePackages/node-env.nix \
-                    --pkg-name nodejs
-                '')
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { moduleWithSystem, ... }:
+      {
+        flake = {
+          homeManagerModules.default = moduleWithSystem (
+            { config, ... }:
+            _: {
+              imports = [
+                ./modules/home-manager.nix
               ];
-            in
-            pkgs.mkShell {
-              packages =
-                scripts
-                ++ (with pkgs; [
-                  lua-language-server
-                  node2nix
-                  nvfetcher
-                  stylua
-                ]);
-              shellHook = ''
-                ${config.pre-commit.installationScript}
-              '';
-            };
-          packages =
-            let
-              makeNeovimWrapper = import ./wrapper.nix neovim-nightly pkgs;
-              neovim-nightly = neovim-nightly-overlay.packages.default;
-              neovimConfig = pkgs.callPackage ./config.nix {
-                inherit plugins;
-              };
-              plugins = import ./plugins.nix pkgs;
-              tools = import ./tools.nix pkgs;
-            in
-            {
-              config = neovimConfig;
-              default = pkgs.callPackage (makeNeovimWrapper tools) {
-                viAlias = false;
-                vimAlias = false;
-              };
-            };
-          pre-commit = {
-            check.enable = true;
-            settings = {
-              hooks = {
-                actionlint.enable = true;
-                check-json.enable = true;
-                check-toml.enable = true;
-                editorconfig-checker = {
-                  enable = true;
-                  excludes = [
-                    "Cargo.lock"
-                    "_sources"
-                    "flake.lock"
-                  ];
-                };
-                markdownlint.enable = true;
-                yamlfmt.enable = true;
-                yamllint.enable = true;
-              };
-              src = ./.;
-            };
-          };
-          treefmt = import ./treefmt.nix;
+              ms0503.neovim.package = config.packages.default;
+            }
+          );
+          nixosModules.default = moduleWithSystem (
+            { config, ... }:
+            _: {
+              imports = [
+                ./modules/nixos.nix
+              ];
+              ms0503.neovim.package = config.packages.default;
+            }
+          );
+          overlays = import ./overlays.nix;
         };
-      systems = import systems;
-    };
+        imports = [
+          git-hooks.flakeModule
+          treefmt-nix.flakeModule
+        ];
+        perSystem =
+          {
+            config,
+            inputs',
+            system,
+            ...
+          }:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                fenix.overlays.default
+                self.overlays.default
+              ];
+            };
+          in
+          {
+            _module.args.pkgs = pkgs;
+            devShells.default =
+              let
+                scripts = with pkgs; [
+                  (writeScriptBin "update-srcs" ''
+                    if [[ ! -f $PWD/flake.nix ]]; then
+                      printf "Please run in flake root\n"
+                      exit 1
+                    fi
+                    ${pkgs.nvfetcher}/bin/nvfetcher
+                    ${pkgs.node2nix}/bin/node2nix \
+                      -i pkgs/nodePackages/pkg-list.json \
+                      -o pkgs/nodePackages/pkgs.nix \
+                      -c pkgs/nodePackages/cmp.nix \
+                      -e pkgs/nodePackages/node-env.nix \
+                      --pkg-name nodejs
+                  '')
+                ];
+              in
+              pkgs.mkShell {
+                packages =
+                  scripts
+                  ++ (with pkgs; [
+                    lua-language-server
+                    node2nix
+                    nvfetcher
+                    stylua
+                  ]);
+                shellHook = ''
+                  ${config.pre-commit.installationScript}
+                '';
+              };
+            packages =
+              let
+                makeNeovimWrapper = import ./wrapper.nix neovim-nightly pkgs;
+                neovim-nightly = inputs'.neovim-nightly-overlay.packages.default;
+                neovimConfig = pkgs.callPackage ./config.nix {
+                  inherit plugins;
+                };
+                plugins = import ./plugins.nix pkgs;
+                tools = import ./tools.nix pkgs;
+              in
+              {
+                config = neovimConfig;
+                default = pkgs.callPackage (makeNeovimWrapper tools) {
+                  viAlias = false;
+                  vimAlias = false;
+                };
+              };
+            pre-commit = {
+              check.enable = true;
+              settings = {
+                hooks = {
+                  actionlint.enable = true;
+                  check-json.enable = true;
+                  check-toml.enable = true;
+                  editorconfig-checker = {
+                    enable = true;
+                    excludes = [
+                      "Cargo.lock"
+                      "_sources"
+                      "flake.lock"
+                    ];
+                  };
+                  markdownlint.enable = true;
+                  yamlfmt.enable = true;
+                  yamllint.enable = true;
+                };
+                src = ./.;
+              };
+            };
+            treefmt = import ./treefmt.nix;
+          };
+        systems = import systems;
+      }
+    );
 }
---
+++ modules/nixos.nix
@@ -0,0 +1,46 @@
+{ config, lib, ... }:
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
+  cfg = config.ms0503.neovim;
+in
+{
+  config = mkIf cfg.enable {
+    environment = {
+      systemPackages = [
+        (cfg.package.override {
+          inherit (cfg) viAlias vimAlias;
+        })
+      ];
+      variables = mkIf cfg.defaultEditor {
+        EDITOR = "nvim";
+      };
+    };
+  };
+  options.ms0503.neovim = {
+    defaultEditor = mkOption {
+      default = false;
+      description = "When enabled, installs neovim and configures neovim to be the default editor using the EDITOR environment variable.";
+      type = types.bool;
+    };
+    enable = mkEnableOption "customized neovim";
+    package = mkOption {
+      description = "The customized neovim package to use.";
+      type = types.package;
+    };
+    viAlias = mkOption {
+      default = false;
+      description = "Symlink {command}`vi` to {command}`nvim` binary.";
+      type = types.bool;
+    };
+    vimAlias = mkOption {
+      default = false;
+      description = "Symlink {command}`vim` to {command}`nvim` binary.";
+      type = types.bool;
+    };
+  };
+}
