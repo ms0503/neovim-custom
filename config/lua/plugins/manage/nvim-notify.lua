e097e3312314a75e0e716ac56f91bba2e397a37a
Sora Tonami <ms0503@outlook.com>
2025-05-11 06:12:37 +0900

    fix: config/plugins/manage/nvim-notify: missing theme dependency

--- config/lua/plugins/manage/nvim-notify.lua
+++ config/lua/plugins/manage/nvim-notify.lua
@@ -2,9 +2,15 @@
 -- repo: https://github.com/rcarriga/nvim-notify
 
 return {
+    dependencies = {
+        dir = '@vim_jb_lua@',
+        name = 'vim-jb.lua',
+    },
     dir = '@nvim_notify@',
     name = 'nvim-notify',
-    opts = {
-        background_colour = require('vim-jb.util').GetColors('dark').editor.gui,
-    },
+    opts = function()
+        return {
+            background_colour = require('vim-jb.util').GetColors('dark').editor.gui,
+        }
+    end,
 }
