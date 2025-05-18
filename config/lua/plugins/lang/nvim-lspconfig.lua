b3c221e1910783222345258aa4890a147f5b35b4
Sora Tonami <ms0503@outlook.com>
2025-05-19 01:39:58 +0900

    feature: config: migrate new lsp config

--- config/lua/plugins/lang/nvim-lspconfig.lua
+++ config/lua/plugins/lang/nvim-lspconfig.lua
@@ -1,101 +1,55 @@
 -- LSP configurations
 -- repo: https://github.com/neovim/nvim-lspconfig
 
-local server_list = {
-    'arduino_language_server',
-    'astro',
-    'autotools_ls',
-    'awk_ls',
-    'bashls',
-    'biome',
-    'buf_ls',
-    'bzl',
-    -- 'clangd', -- configured in plugins.lsp.nvim-lspconfig
-    'cmake',
-    'css_variables',
-    -- 'cssls', -- configured in plugins.lsp.nvim-lspconfig
-    'cssmodules_ls',
-    -- 'denols', -- configured in plugins.lsp.nvim-lspconfig
-    'docker_compose_language_service',
-    'dockerls',
-    -- 'eslint', -- configured in plugins.lsp.nvim-lspconfig
-    'gopls',
-    -- 'groovyls',
-    'hls',
-    'html',
-    'java_language_server',
-    -- 'jsonls', -- configured in plugins.lsp.nvim-lspconfig
-    'kotlin_language_server',
-    -- 'lua_ls', -- configured in plugins.lsp.nvim-lspconfig
-    'marksman',
-    'mesonlsp',
-    'nginx_language_server',
-    'nil_ls',
-    'nushell',
-    'ocamllsp',
-    'omnisharp',
-    'phpactor',
-    -- 'pkgbuild_language_server',
-    -- 'powershell_es',
-    -- 'prismals',
-    'pyright',
-    'ruff',
-    'rust_analyzer',
-    'stylelint_lsp',
-    'taplo',
-    'tailwindcss',
-    'terraformls',
-    -- 'tinymist', -- configured in plugins.lsp.nvim-lspconfig
-    -- 'ts_ls', -- configured in plugins.lsp.nvim-lspconfig
-    'vuels',
-    -- 'yamlls', -- configured in plugins.lsp.nvim-lspconfig
-    'zls',
-}
+local utils = require('utils')
 
-return {
-    config = function()
-        local lspconfig = require('lspconfig')
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.completion.completionItem.snippetSupport =
-            true
-        vim.lsp.handlers['textDocument/publishDiagnostics'] = function(
-            err,
-            result,
-            ctx,
-            config
-        )
-            require('ts-error-translator').translate_diagnostics(
-                err,
-                result,
-                ctx
-            )
-            vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
-        end
-        for _, server in ipairs(server_list) do
-            lspconfig[server].setup({})
-        end
-        local node_root_dir = lspconfig.util.root_pattern('package.json')
-        local is_node_repo = node_root_dir(vim.api.nvim_buf_get_name(0)) ~= nil
-        if is_node_repo then
-            lspconfig.ts_ls.setup({})
-        else
-            lspconfig.denols.setup({
-                init_options = {
-                    lint = true,
-                    suggest = {
-                        imports = {
-                            hosts = {
-                                ['https://cdn.nest.land'] = true,
-                                ['https://crux.land'] = true,
-                                ['https://deno.land'] = true,
-                            },
+local keys = utils.keys
+
+local server_list = function()
+    local schemastore = require('schemastore')
+    return {
+        arduino_language_server = {},
+        astro = {},
+        autotools_ls = {},
+        awk_ls = {},
+        bashls = {},
+        biome = {},
+        buf_ls = {},
+        bzl = {},
+        clangd = {
+            capabilities = capabilities,
+            filetypes = {
+                'c',
+                'cpp',
+                'cuda',
+                'objc',
+                'objcpp',
+            },
+        },
+        cmake = {},
+        css_variables = {},
+        cssls = {
+            capabilities = capabilities,
+        },
+        cssmodules_ls = {},
+        denols = {
+            init_options = {
+                lint = true,
+                suggest = {
+                    imports = {
+                        hosts = {
+                            ['https://cdn.nest.land'] = true,
+                            ['https://crux.land'] = true,
+                            ['https://deno.land'] = true,
                         },
                     },
-                    unstable = true,
                 },
-            })
-        end
-        lspconfig.eslint.setup({
+                unstable = true,
+            },
+        },
+        docker_compose_language_service = {},
+        dockerls = {},
+        eslint = {
             settings = {
                 codeActionOnSave = {
                     enable = true,
@@ -105,43 +59,24 @@ return {
                 },
                 format = true,
             },
-        })
-        lspconfig.cssls.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.jsonls.setup({
+        },
+        gopls = {},
+        hls = {},
+        html = {},
+        java_language_server = {},
+        jsonls = {
             capabilities = capabilities,
             settings = {
                 json = {
-                    schemas = require('schemastore').json.schemas(),
+                    schemas = schemastore.json.schemas(),
                     validate = {
                         enable = true,
                     },
                 },
             },
-        })
-        lspconfig.yamlls.setup({
-            settings = {
-                yaml = {
-                    schemaStore = {
-                        enable = false,
-                        url = '',
-                    },
-                    schemas = require('schemastore').yaml.schemas(),
-                },
-            },
-        })
-        lspconfig.clangd.setup({
-            capabilities = capabilities,
-            filetypes = {
-                'c',
-                'cpp',
-                'cuda',
-                'objc',
-                'objcpp',
-            },
-        })
-        lspconfig.lua_ls.setup({
+        },
+        kotlin_language_server = {},
+        lua_ls = {
             settings = {
                 Lua = {
                     diagnostics = {
@@ -151,13 +86,76 @@ return {
                     },
                 },
             },
-        })
-        lspconfig.tinymist.setup({
+        },
+        marksman = {},
+        mesonlsp = {},
+        nginx_language_server = {},
+        nil_ls = {},
+        nushell = {},
+        ocamllsp = {},
+        omnisharp = {},
+        phpactor = {},
+        pkgbuild_language_server = {},
+        powershell_es = {},
+        prismals = {},
+        pyright = {},
+        ruff = {},
+        rust_analyzer = {},
+        stylelint_lsp = {},
+        taplo = {},
+        tailwindcss = {},
+        terraformls = {},
+        tinymist = {
             offset_encoding = 'utf-8',
             settings = {
                 formatterMode = 'typstyle',
             },
-        })
+        },
+        ts_ls = {},
+        vuels = {},
+        yamlls = {
+            settings = {
+                yaml = {
+                    schemaStore = {
+                        enable = false,
+                        url = '',
+                    },
+                    schemas = schemastore.yaml.schemas(),
+                },
+            },
+        },
+        zls = {},
+    }
+end
+
+return {
+    config = function()
+        local lspconfig = require('lspconfig')
+        local ts_error_translator = require('ts-error-translator')
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        local server_list = server_list()
+        capabilities.textDocument.completion.completionItem.snippetSupport =
+            true
+        vim.lsp.handlers['textDocument/publishDiagnostics'] = function(
+            err,
+            result,
+            ctx,
+            config
+        )
+            ts_error_translator.translate_diagnostics(err, result, ctx)
+            vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
+        end
+        for name, opts in pairs(server_list) do
+            vim.lsp.config(name, opts)
+        end
+        vim.lsp.enable(keys(server_list))
+        local node_root_dir = lspconfig.util.root_pattern('package.json')
+        local is_node_repo = node_root_dir(vim.api.nvim_buf_get_name(0)) ~= nil
+        if is_node_repo then
+            vim.lsp.enable('denols', false)
+        else
+            vim.lsp.enable('ts_ls', false)
+        end
     end,
     dependencies = {
         {
--- config/lua/utils.lua
+++ config/lua/utils.lua
@@ -1,4 +1,5 @@
 local M = {}
+
 local icons = {
     ActiveLSP = '',
     ActiveTS = '',
@@ -61,9 +62,55 @@ local icons = {
     Window = '',
     WordFile = '󰈭',
 }
---! @param icon_name string
---! @return string
+
+--- Filter array, like JavaScript's Array.prototype.filter
+---
+---@generic T
+---@param arr T[]
+---@param filterFn fun(v:any, i:number, arr:T[]):bool
+---@return T[]
+function M.filter(arr, filterFn)
+    local result = {}
+    for i, v in ipairs(arr) do
+        if filterFn(v, i, arr) then
+            table.insert(result, v)
+        end
+    end
+    return result
+end
+
+--- Get icon from icon set
+---
+---@param icon_name string
+---@return string
 function M.get_icon(icon_name)
     return icons[icon_name] or ''
 end
+
+--- Get table keys
+---
+---@param tbl table
+---@return (number|string)[]
+function M.keys(tbl)
+    local result = {}
+    for k, _ in pairs(tbl) do
+        table.insert(result, k)
+    end
+    return result
+end
+
+--- Map array, like JavaScript's Array.prototype.map
+---
+---@generic T, U
+---@param arr T[]
+---@param mapFn fun(v:any, i:number, arr:T[]):U
+---@return U[]
+function M.map(arr, mapFn)
+    local result = {}
+    for i, v in ipairs(arr) do
+        result[i] = mapFn(v, i, arr)
+    end
+    return result
+end
+
 return M
