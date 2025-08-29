5ae2b1144d25a844c0981417561bceb7cf41ca2d
Sora Tonami <ms0503@outlook.com>
2025-08-29 21:41:46 +0900

    fix: workaround NixOS/nixpkgs#437024

--- nix/tools.nix
+++ nix/tools.nix
@@ -2,6 +2,30 @@ pkgs: myPkgs: inputs':
 let
   fenixPkgs = inputs'.fenix.packages;
 in
+let
+  # https://github.com/nixos/nixpkgs/issues/437024
+  cmake-language-server' = pkgs.cmake-language-server.override {
+    pygls = pygls';
+  };
+  lsprotocol' = pkgs.python3Packages.lsprotocol.overridePythonAttrs {
+    src = pkgs.fetchFromGitHub {
+      hash = "sha256-PHjLKazMaT6W4Lve1xNxm6hEwqE3Lr2m5L7Q03fqb68=";
+      owner = "microsoft";
+      repo = "lsprotocol";
+      tag = "2023.0.1";
+    };
+    version = "2023.0.1";
+  };
+  nginx-language-server' = pkgs.nginx-language-server.override {
+    python3Packages = pkgs.python3Packages // {
+      lsprotocol = lsprotocol';
+      pygls = pygls';
+    };
+  };
+  pygls' = pkgs.python3Packages.pygls.override {
+    lsprotocol = lsprotocol';
+  };
+in
 (
   with pkgs;
   [
@@ -11,7 +35,7 @@ in
     biome
     buf
     clang-tools
-    cmake-language-server
+    cmake-language-server'
     delve
     deno
     docker-compose-language-service
@@ -24,7 +48,7 @@ in
     lua-language-server
     marksman
     mesonlsp
-    nginx-language-server
+    nginx-language-server'
     nil
     nixfmt-rfc-style
     nushell
