4a064b0ab448a19e1cf24e0e2bacce109dc8c5a5
Sora Tonami <ms0503@outlook.com>
2026-06-03 23:38:13 +0900

    chore: make git-hooks.nix and treefmt-nix optional

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,31 +1,33 @@
 { inputs, ... }:
 {
   imports = [
-    inputs.treefmt-nix.flakeModule
+    (inputs.treefmt-nix.flakeModule or { })
   ];
-  perSystem = _: {
-    treefmt.programs = {
-      nixfmt = {
-        enable = true;
-        excludes = [
-          "_sources/generated.nix"
-          "nix/pkgs/nodePackages/cmp.nix"
-          "nix/pkgs/nodePackages/node-env.nix"
-          "nix/pkgs/nodePackages/pkgs.nix"
-        ];
+  perSystem =
+    { lib, ... }:
+    lib.optionalAttrs (inputs.treefmt-nix ? flakeModule) {
+      treefmt.programs = {
+        nixfmt = {
+          enable = true;
+          excludes = [
+            "_sources/generated.nix"
+            "nix/pkgs/nodePackages/cmp.nix"
+            "nix/pkgs/nodePackages/node-env.nix"
+            "nix/pkgs/nodePackages/pkgs.nix"
+          ];
+        };
+        shellcheck = {
+          enable = true;
+          excludes = [
+            ".envrc"
+          ];
+        };
+        shfmt = {
+          enable = true;
+          indent_size = 4;
+        };
+        stylua.enable = true;
+        taplo.enable = true;
       };
-      shellcheck = {
-        enable = true;
-        excludes = [
-          ".envrc"
-        ];
-      };
-      shfmt = {
-        enable = true;
-        indent_size = 4;
-      };
-      stylua.enable = true;
-      taplo.enable = true;
     };
-  };
 }
